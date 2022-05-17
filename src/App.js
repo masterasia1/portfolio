608dd5b5aa9163446b1a4522757c32694e32a916
masterasia1 <a.mayfield18@gmail.com>
2022-05-17 13:48:07 -0700

    add photo and CSS

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css'
 import image from './images/img-1.png'
+import {Container, Button,  Card} from 'react-bootstrap';
 
 function App() {
 
 
   return (
     <div>
+   
    <div className='headerStyle'>
      <header>
        <h2>Asia Mayfield</h2>
        <p>Frontend Software Engineer | React Developer</p>
      </header>
    </div>
-   <br />
-   <br />
-   <img src={image} alt="Girl in a jacket"></img>
+   
+   <img src={image} alt="Profile"></img>
+
+   <div className='bodyStyle'>
+     <h1>About</h1>
+     <p>blavj blavj </p>
+
+
+
+
+
+   </div>
+
+
    </div>
 
   );
